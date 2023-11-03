43f81b2e800c1f15e565029201836dc7b6106308
GraydonvG <graydonvg@gmail.com>
2023-11-03 18:19:46 +0200

    adding try catch

--- src/services/revalidate.ts
+++ src/services/revalidate.ts
@@ -2,9 +2,14 @@ import getURL from '@/lib/utils';
 
 export default async function revalidate(path: string) {
   const url = getURL(path);
-  const response = await fetch(`${url}?path=/&secret=${process.env.NEXT_PUBLIC_ON_DEMAND_REVALIDATION_SECRET_TOKEN}`);
 
-  const data = await response.json();
+  try {
+    const response = await fetch(`${url}?path=/&secret=${process.env.NEXT_PUBLIC_ON_DEMAND_REVALIDATION_SECRET_TOKEN}`);
 
-  return data;
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw new Error(`@services/revalidate. ${error}`);
+  }
 }
