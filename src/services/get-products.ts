338afd4d21a97b55b6633ac1665e8f954635a6b2
GraydonvG <graydonvg@gmail.com>
2023-11-03 18:19:46 +0200

    adding try catch

--- src/services/get-products.ts
+++ src/services/get-products.ts
@@ -3,10 +3,15 @@ import { CustomResponseType, ProductType } from '@/types';
 
 export default async function getAllProducts(): Promise<CustomResponseType<ProductType[]>> {
   const url = getURL('/api/products/get-all');
-  const response = await fetch(url, {
-    cache: 'force-cache',
-  });
-  const data = await response.json();
 
-  return data;
+  try {
+    const response = await fetch(url, {
+      cache: 'force-cache',
+    });
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw new Error(`@services/get-products. ${error}`);
+  }
 }
