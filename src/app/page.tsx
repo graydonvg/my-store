e7929887a3322b73963dd558180dc4bf43e6c45f
GraydonvG <graydonvg@gmail.com>
2023-11-07 17:16:10 +0200

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import Products from '@/components/Products';
-import serverClient from '@/lib/supabase-server';
 import getAllProducts from '@/services/get-all-products';
 import { ProductType } from '@/types';
 import { Typography } from '@mui/material';
 
 export default async function Home() {
-  const supabase = await serverClient();
   // const { data: products } = await getAllProducts();
-  const { data: products, error } = await supabase
-    .from('products')
-    .select('*, product_image_data(file_name, image_url, product_image_id, index)')
-    .order('created_at', { ascending: false });
+  const res = await fetch('http://localhost:3000/api/products/get-all', {
+    method: 'GET',
+    cache: 'no-store',
+  });
+  const { data: products } = await res.json();
+
+  // console.log(products);
 
   return (
     <>
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -9,40 +9,41 @@ type Props = {
 
 export default function Products({ products }: Props) {
   return (
-    // <Grid
-    //   container
-    //   spacing={4}>
-    //   {categories?.map((category) => {
-    //     return (
-    //       <Grid
-    //         xs={12}
-    //         item
-    //         key={category}>
-    //         <Typography
-    //           sx={{ textAlign: 'center', paddingBottom: 2 }}
-    //           component="h2"
-    //           variant="h5">
-    //           {category}
-    //         </Typography>
     <Grid
       container
-      spacing={{ xs: 0.3, md: 2 }}>
-      {products.map((product, index) => (
-        // return product.category === category ? (
-        <Grid
-          key={index}
-          item
-          xs={6}
-          md={4}
-          lg={3}
-          xl={2}>
-          <ProductCard product={product} />
-        </Grid>
-      ))}
+      spacing={4}>
+      {categories?.map((category) => {
+        return (
+          <Grid
+            xs={12}
+            item
+            key={category}>
+            <Typography
+              sx={{ textAlign: 'center', paddingBottom: 2 }}
+              component="h2"
+              variant="h5">
+              {category}
+            </Typography>
+            <Grid
+              container
+              spacing={1}>
+              {products.map((product, index) => {
+                return product.category === category ? (
+                  <Grid
+                    key={index}
+                    item
+                    xs={6}
+                    md={4}
+                    lg={3}
+                    xl={2}>
+                    <ProductCard product={product} />
+                  </Grid>
+                ) : null;
+              })}
+            </Grid>
+          </Grid>
+        );
+      })}
     </Grid>
-    //       </Grid>
-    //     );
-    //   })}
-    // </Grid>
   );
 }
