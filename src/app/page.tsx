f4eac973b9312c34cca23160595f272815d85bc2
GraydonvG <graydonvg@gmail.com>
2023-11-07 16:24:58 +0200

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Products from '@/components/Products';
+import serverClient from '@/lib/supabase-server';
 import getAllProducts from '@/services/get-all-products';
 import { ProductType } from '@/types';
 import { Typography } from '@mui/material';
 
 export default async function Home() {
-  const { data: products } = await getAllProducts();
+  const supabase = await serverClient();
+  // const { data: products } = await getAllProducts();
+  const { data: products, error } = await supabase
+    .from('products')
+    .select('*, product_image_data(file_name, image_url, product_image_id, index)')
+    .order('created_at', { ascending: false });
 
   return (
     <>
