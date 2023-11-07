458dc8554690f2b0a2f9daca0e2736771a91980b
GraydonvG <graydonvg@gmail.com>
2023-11-07 17:54:37 +0200

    bug fix

--- src/services/get-all-products.ts
+++ src/services/get-all-products.ts
@@ -2,7 +2,7 @@ import getURL from '@/lib/utils';
 import { CustomResponseType, ProductType } from '@/types';
 
 export default async function getAllProducts(): Promise<CustomResponseType<ProductType[]>> {
-  const url = getURL('/api/test-route');
+  const url = getURL('/api/products/get-all');
 
   try {
     const response = await fetch(url, {
