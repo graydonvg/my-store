fdf27c7cfdde2e0df4defa80b037d18fc7de6493
GraydonvG <graydonvg@gmail.com>
2023-11-13 14:26:59 +0200

    update db image index if change on product update

---
+++ src/services/product-image-data/update-product-image-data.ts
@@ -0,0 +1,21 @@
+import { CustomResponseType, ProductImageDataStoreType } from '@/types';
+
+export default async function updateProductImageData(
+  imageData: ProductImageDataStoreType[]
+): Promise<CustomResponseType> {
+  try {
+    const response = await fetch('/api/product-image-data/update', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(imageData),
+    });
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw new Error(`@services/update-product-image-data. ${error}`);
+  }
+}
