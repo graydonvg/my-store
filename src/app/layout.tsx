d42265d8f56c2571e640034504a0ce1b6096ebbe
GraydonvG <graydonvg@gmail.com>
2023-12-20 15:54:02 +0200

    updating nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,6 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
       .select(
         'created_at, cart_item_id, quantity, size, product: products!inner(name, on_sale, price, sale_percentage, delivery_info, return_info, product_id, sizes,product_image_data!inner(image_url))'
       )
-      // .eq('products.product_image_data.index', 0)
       .order('created_at', { ascending: false });
 
     cartItems = cart ? cart : ([] as CartItemType[]);
