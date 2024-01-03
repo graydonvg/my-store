18820188b8e905a6b94a0bf987f86547edbeb212
GraydonvG <graydonvg@gmail.com>
2024-01-03 13:22:28 +0200

    amount rounded correctly for stripe

--- src/utils/payWithStripe.ts
+++ src/utils/payWithStripe.ts
@@ -2,14 +2,15 @@ import { loadStripe } from '@stripe/stripe-js';
 import { calculateDiscountedCartItemPrice } from './calculateDiscountedPrice';
 import { callStripeSession } from '@/services/stripe/call-stripe-session';
 import { CartItemType } from '@/types';
+import { toast } from 'react-toastify';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 export default async function payWithStripe(cartItems: CartItemType[]) {
   const stripe = await stripePromise;
   const createLineItems = cartItems.map((item) => {
-    const unitAmount = (item?.product?.on_sale ? calculateDiscountedCartItemPrice(item) : item?.product?.price!) * 100;
-    const roundedAmount = Math.round(unitAmount);
+    const unitAmount = item?.product?.on_sale ? calculateDiscountedCartItemPrice(item) : item?.product?.price!;
+    const roundedAmount = Math.round(unitAmount) * 100;
     const images = item?.product?.product_image_data.map((data) => data.image_url);
 
     return {
@@ -31,5 +32,7 @@ export default async function payWithStripe(cartItems: CartItemType[]) {
     sessionId: data?.sessionId!,
   });
 
-  console.log(error);
+  if (error) {
+    toast.error(error.error.message);
+  }
 }
