7b107d6725d11f5de051cea844fa2bd381e48faf
GraydonvG <graydonvg@gmail.com>
2024-01-03 09:25:02 +0200

    fixing missing prop

--- src/app/admin-view/all-products/page.tsx
+++ src/app/admin-view/all-products/page.tsx
@@ -6,12 +6,13 @@ import { Box } from '@mui/material';
 export default async function AdminViewAllProducts() {
   const { data: products } = await getAllProducts();
 
-  if (!products) return null;
-
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
       <RevalidateButton />
-      <Products products={products} />
+      <Products
+        show={!!products}
+        products={products!}
+      />
     </Box>
   );
 }
