a998a3b9c4a8850c2e4697db18063889480097a9
GraydonvG <graydonvg@gmail.com>
2024-03-08 15:32:55 +0200

    replacing framer-motion with dnd-kit

--- src/components/draggableProductImages/DraggableProductImage.tsx
+++ src/components/draggableProductImages/DraggableProductImage.tsx
@@ -9,39 +9,38 @@ import { toast } from 'react-toastify';
 import { deleteProductImageFromStorage } from '@/lib/firebase';
 import deleteProductImageDataFromDb from '@/services/product-image-data/delete';
 import { useAppDispatch, useAppSelector } from '@/lib/redux/hooks';
-import { PointerEvent, useState } from 'react';
+import { useState } from 'react';
 import { deleteImageData, setIsDeletingImage } from '@/lib/redux/slices/productImagesSlice';
-import { Reorder, useDragControls, useMotionValue } from 'framer-motion';
-import { useBackgroundColorOnDrag } from '@/hooks/useBackgroundColorOnDrag';
+import { UniqueIdentifier } from '@dnd-kit/core';
+import { CSS } from '@dnd-kit/utilities';
+import { useSortable } from '@dnd-kit/sortable';
 
 export type Props = {
-  imageData: InsertProductImageDataTypeStore;
-  arrayIndex: number;
+  imageData: InsertProductImageDataTypeStore & { id: string };
+  activeItemId: UniqueIdentifier | null;
 };
 
-export default function DraggableProductImage({ imageData, arrayIndex }: Props) {
-  const y = useMotionValue(1);
-  const backgroundColorOnDrag = useBackgroundColorOnDrag(y);
-  const dragControls = useDragControls();
+export default function DraggableProductImage({ imageData, activeItemId }: Props) {
   const dispatch = useAppDispatch();
   const colorPalette = useColorPalette();
   const { productFormData } = useAppSelector((state) => state.productForm);
   const isDeletingImage = useAppSelector((state) => state.productImages.isDeletingImage);
-  const [imageToDeleteIndex, setImageToDeleteIndex] = useState<number | null>(null);
+  const [imageToDeleteId, setImageToDeleteId] = useState<string | null>(null);
   const [isImageLoaded, setIsImageLoaded] = useState(false);
-  const isDeletingCurrentImage = isDeletingImage && imageToDeleteIndex === arrayIndex;
-
-  function handleDragStart(e: PointerEvent<HTMLButtonElement>) {
-    if (window.navigator.vibrate) {
-      window.navigator.vibrate(100);
-    }
-
-    dragControls.start(e);
-  }
+  const isDeletingCurrentImage = isDeletingImage && imageToDeleteId === imageData.id;
+  const {
+    attributes: { role, ...restOfAttributes },
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({
+    id: imageData.id,
+  });
 
   async function deleteImage() {
     dispatch(setIsDeletingImage(true));
-    setImageToDeleteIndex(imageData.index);
+    setImageToDeleteId(imageData.id);
 
     if (imageData.fileName.length > 0) {
       await deleteProductImageFromStorage(imageData.fileName);
@@ -56,90 +55,86 @@ export default function DraggableProductImage({ imageData, arrayIndex }: Props) 
     }
 
     dispatch(deleteImageData({ fileName: imageData.fileName }));
-    setImageToDeleteIndex(null);
+    setImageToDeleteId(null);
     dispatch(setIsDeletingImage(false));
   }
 
   return (
     <>
-      <Reorder.Item
-        value={imageData}
-        dragListener={false}
-        dragControls={dragControls}
-        style={{ position: 'relative', backgroundColor: backgroundColorOnDrag, y }}>
+      <Grid
+        container
+        ref={setNodeRef}
+        sx={{
+          borderRadius: BORDER_RADIUS,
+          paddingY: 2,
+          opacity: isDeletingCurrentImage ? 0.5 : 1,
+          backgroundColor: imageData.id === activeItemId ? colorPalette.boxShadow : 'transparent',
+          transform: CSS.Translate.toString(transform),
+          transition,
+        }}>
         <Grid
-          container
-          sx={{
-            borderRadius: BORDER_RADIUS,
-            paddingY: 2,
-            opacity: isDeletingCurrentImage ? 0.5 : 1,
-          }}>
-          <Grid
-            item
-            xs={2}
-            sx={{ display: 'grid', placeItems: 'center' }}>
-            <IconButton
-              aria-label="drag handle"
-              onPointerDown={(e) => handleDragStart(e)}
-              disableRipple
-              sx={{ cursor: 'grab', touchAction: 'none' }}>
-              <DragHandle fontSize="large" />
-            </IconButton>
-          </Grid>
-          <Grid
-            item
-            xs={4}
-            sx={{ display: 'grid' }}>
-            <Box
-              sx={{
-                position: 'relative',
-                aspectRatio: 3 / 4,
-                alignSelf: 'center',
-              }}>
-              <Image
-                style={{
-                  objectFit: 'cover',
-                  borderRadius: BORDER_RADIUS,
-                  opacity: !isImageLoaded ? 0 : 100,
-                }}
-                fill
-                sizes="(min-width: 600px) 100px, calc(35vw - 25px)"
-                src={imageData.imageUrl}
-                alt={`Image for ${imageData.fileName}`}
-                onLoad={() => setIsImageLoaded(true)}
-              />
-              {!isImageLoaded ? (
-                <Skeleton
-                  height="100%"
-                  width="100%"
-                  variant="rectangular"
-                />
-              ) : null}
-            </Box>
-          </Grid>
-          <Grid
-            item
-            xs={6}>
-            <Box
-              sx={{
-                height: 1,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <TextButton
-                label={isDeletingCurrentImage ? '' : 'delete'}
-                onClick={deleteImage}
-                isLoading={isDeletingCurrentImage}
-                disabled={isDeletingCurrentImage}
-                labelColor={colorPalette.typography}
-                startIcon={<DeleteForever />}
-              />
-            </Box>
-          </Grid>
+          item
+          xs={2}
+          sx={{ display: 'grid', placeItems: 'center' }}>
+          <IconButton
+            {...listeners}
+            {...restOfAttributes}
+            disableRipple
+            sx={{ cursor: 'grab', touchAction: 'none' }}>
+            <DragHandle fontSize="large" />
+          </IconButton>
         </Grid>
-      </Reorder.Item>
+        <Grid
+          item
+          xs={4}
+          sx={{ display: 'grid' }}>
+          <Box
+            sx={{
+              position: 'relative',
+              aspectRatio: 3 / 4,
+              alignSelf: 'center',
+            }}>
+            <Image
+              style={{
+                objectFit: 'cover',
+                borderRadius: BORDER_RADIUS,
+                opacity: !isImageLoaded ? 0 : 100,
+              }}
+              fill
+              sizes="(min-width: 600px) 100px, calc(35vw - 25px)"
+              src={imageData.imageUrl}
+              alt={`Image for ${imageData.fileName}`}
+              onLoad={() => setIsImageLoaded(true)}
+            />
+            {!isImageLoaded ? (
+              <Skeleton
+                height="100%"
+                width="100%"
+                variant="rectangular"
+              />
+            ) : null}
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={6}>
+          <Box
+            sx={{
+              height: 1,
+              display: 'grid',
+              placeItems: 'center',
+            }}>
+            <TextButton
+              label={!isDeletingCurrentImage ? 'delete' : ''}
+              onClick={deleteImage}
+              isLoading={isDeletingCurrentImage}
+              disabled={isDeletingCurrentImage}
+              labelColor={colorPalette.typography}
+              startIcon={<DeleteForever />}
+            />
+          </Box>
+        </Grid>
+      </Grid>
       <Divider />
     </>
   );
--- src/components/draggableProductImages/DraggableProductImages.tsx
+++ src/components/draggableProductImages/DraggableProductImages.tsx
@@ -1,35 +1,71 @@
 import DraggableProductImage from './DraggableProductImage';
 import { useAppDispatch, useAppSelector } from '@/lib/redux/hooks';
 import { setUpdatedImageData } from '@/lib/redux/slices/productImagesSlice';
-import { Reorder } from 'framer-motion';
-import { InsertProductImageDataTypeStore } from '@/types';
+import { DndContext, DragEndEvent, UniqueIdentifier, closestCenter } from '@dnd-kit/core';
+import { SortableContext, verticalListSortingStrategy, arrayMove } from '@dnd-kit/sortable';
+import { restrictToVerticalAxis } from '@dnd-kit/modifiers';
+import { Box } from '@mui/material';
+import { useState } from 'react';
 
 type Props = {
   isDeletingAllImages: boolean;
 };
 
 export default function DraggableProductImages({ isDeletingAllImages }: Props) {
+  const [activeItemId, setActiveItemId] = useState<UniqueIdentifier | null>(null);
   const dispatch = useAppDispatch();
   const imageData = useAppSelector((state) => state.productImages.imageData);
+  const imageDataWithId = imageData.map((data) => {
+    return { ...data, id: data.fileName };
+  });
 
-  function setReorderedImageData(reorderedImageData: InsertProductImageDataTypeStore[]) {
-    dispatch(setUpdatedImageData(reorderedImageData));
+  function handleDragStart(event: DragEndEvent) {
+    setActiveItemId(event.active.id);
+
+    if (window.navigator.vibrate) {
+      window.navigator.vibrate(100);
+    }
+  }
+
+  function handleDragEnd(event: DragEndEvent) {
+    setActiveItemId(null);
+
+    const { active, over } = event;
+
+    if (!over || active.id === over.id) return;
+
+    const activeIndex = imageDataWithId.findIndex((data) => data.id === active.id);
+    const overIndex = imageDataWithId.findIndex((data) => data.id === over.id);
+
+    const sortedImageData = arrayMove(imageDataWithId, activeIndex, overIndex);
+
+    const imageDataWithNoId = sortedImageData.map((data) => {
+      const { id, ...restOfData } = data;
+      return restOfData;
+    });
+
+    dispatch(setUpdatedImageData(imageDataWithNoId));
   }
 
   return (
-    <Reorder.Group
-      axis="y"
-      values={imageData}
-      layoutScroll
-      style={{ overflowY: 'auto', opacity: isDeletingAllImages ? 0.5 : 1, height: '100%' }}
-      onReorder={(reorderedImageData) => setReorderedImageData(reorderedImageData)}>
-      {imageData.map((data, index) => (
-        <DraggableProductImage
-          key={data.fileName}
-          imageData={data}
-          arrayIndex={index}
-        />
-      ))}
-    </Reorder.Group>
+    <Box style={{ overflowY: 'auto', opacity: isDeletingAllImages ? 0.5 : 1, height: '100%' }}>
+      <DndContext
+        collisionDetection={closestCenter}
+        modifiers={[restrictToVerticalAxis]}
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}>
+        <SortableContext
+          items={imageDataWithId}
+          strategy={verticalListSortingStrategy}>
+          {imageDataWithId.map((data) => (
+            <DraggableProductImage
+              key={data.fileName}
+              imageData={data}
+              activeItemId={activeItemId}
+            />
+          ))}
+        </SortableContext>
+      </DndContext>
+    </Box>
   );
 }
