87a1ff0fbf8ccb0d792915745c451de6abb3a429
GraydonvG <graydonvg@gmail.com>
2024-05-14 16:02:32 +0200

    adding cart to store persist whitelist

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -20,7 +20,7 @@ type ExtendedPersistConfig = PersistConfig<RootState> & {
 const persistConfig: ExtendedPersistConfig = {
   key: 'root',
   storage,
-  whitelist: ['theme', 'productForm', 'productImages', 'checkoutData', 'user'],
+  whitelist: ['theme', 'productForm', 'productImages', 'checkoutData', 'cart', 'user'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
