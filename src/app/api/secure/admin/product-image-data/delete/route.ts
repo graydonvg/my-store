9ff136e105ee41c7f9e2a91fe359fe09578221ac
GraydonvG <graydonvg@gmail.com>
2024-06-18 15:48:42 +0200

    add validation and logger to delete product image data route handler

--- src/app/api/secure/admin/product-image-data/delete/route.ts
+++ src/app/api/secure/admin/product-image-data/delete/route.ts
@@ -1,45 +1,115 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { CustomResponse } from '@/types';
-import { ERROR_MESSAGES } from '@/constants';
+import { NextResponse } from 'next/server';
+import { NumericIdSchema, ResponseWithNoData } from '@/types';
+import { CONSTANTS } from '@/constants';
 import createSupabaseServerClient from '@/lib/supabase/supabase-server';
+import { AxiomRequest, withAxiom } from 'next-axiom';
 
-export async function DELETE(request: NextRequest): Promise<NextResponse<CustomResponse>> {
+export const DELETE = withAxiom(async (request: AxiomRequest): Promise<NextResponse<ResponseWithNoData>> => {
   const supabase = await createSupabaseServerClient();
+  let log = request.log;
+
+  log.info('Attempting to delete product image data from db');
 
   try {
     const {
       data: { user: authUser },
+      error: authError,
     } = await supabase.auth.getUser();
+
+    if (authError) {
+      log.error(CONSTANTS.LOGGER_ERROR_MESSAGES.AUTHENTICATION, { error: authError });
+
+      return NextResponse.json(
+        {
+          success: false,
+          message: CONSTANTS.USER_ERROR_MESSAGES.AUTHENTICATION,
+        },
+        { status: 500 }
+      );
+    }
+
+    if (!authUser) {
+      log.warn(CONSTANTS.LOGGER_ERROR_MESSAGES.NOT_AUTHENTICATED, { user: authUser });
+
+      return NextResponse.json(
+        {
+          success: false,
+          message: CONSTANTS.USER_ERROR_MESSAGES.NOT_AUTHENTICATED,
+        },
+        { status: 401 }
+      );
+    }
+
+    log = request.log.with({ userId: authUser.id });
 
     const searchParams = request.nextUrl.searchParams;
     const productImageId = searchParams.get('product_image_id');
 
-    if (!authUser)
-      return NextResponse.json({
-        success: false,
-        message: `Failed to delete image data from database. ${ERROR_MESSAGES.NOT_AUTHENTICATED}`,
-      });
+    if (!productImageId) {
+      log.error(CONSTANTS.LOGGER_ERROR_MESSAGES.NO_DATA);
 
-    if (!productImageId)
-      return NextResponse.json({
-        success: false,
-        message: `Failed to delete image data from database. ${ERROR_MESSAGES.NO_ID_RECEIVED}`,
-      });
-
-    const { error } = await supabase.from('productImageData').delete().eq('productImageId', productImageId);
-
-    if (error) {
-      return NextResponse.json({
-        success: false,
-        message: `Failed to delete image data from database. ${error.message}.`,
-      });
+      return NextResponse.json(
+        {
+          success: false,
+          message: CONSTANTS.USER_ERROR_MESSAGES.UNEXPECTED,
+        },
+        { status: 400 }
+      );
     }
 
-    return NextResponse.json({ success: true, message: 'Product image data deleted successfully' });
+    const validation = NumericIdSchema.safeParse(productImageId);
+
+    if (!validation.success) {
+      log.error(CONSTANTS.LOGGER_ERROR_MESSAGES.VALIDATION, { data: productImageId, error: validation.error });
+
+      return NextResponse.json(
+        {
+          success: false,
+          message: CONSTANTS.USER_ERROR_MESSAGES.UNEXPECTED,
+        },
+        { status: 400 }
+      );
+    }
+
+    const { error: deleteError } = await supabase
+      .from('productImageData')
+      .delete()
+      .eq('productImageId', validation.data);
+
+    if (deleteError) {
+      log.error(CONSTANTS.LOGGER_ERROR_MESSAGES.DATABASE_DELETE, { error: deleteError });
+
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Failed to delete product image data from database. Please try again later.',
+        },
+        { status: 500 }
+      );
+    }
+
+    const successMessage = 'Product image data deleted successfully';
+
+    log.info(successMessage);
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: successMessage,
+      },
+      { status: 200 }
+    );
   } catch (error) {
-    return NextResponse.json({
-      success: false,
-      message: 'Failed to delete image data from database. An unexpect error occured.',
-    });
+    log.error(CONSTANTS.LOGGER_ERROR_MESSAGES.UNEXPECTED, { error });
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: CONSTANTS.USER_ERROR_MESSAGES.UNEXPECTED,
+      },
+      { status: 500 }
+    );
+  } finally {
+    await log.flush();
   }
-}
+});
