3fdcf72a174a63266a142c82e8a1c163cad95973
GraydonvG <graydonvg@gmail.com>
2024-09-22 11:00:32 +0200

    add link to back to order item image

--- src/components/ordersPageStorefront/orderItems/OrderItemImage.tsx
+++ src/components/ordersPageStorefront/orderItems/OrderItemImage.tsx
@@ -18,39 +18,45 @@ export default function OrderItemImage({ imageUrl, orderItem }: Props) {
   });
 
   return (
-    <Box
-      // Attach the ref to track when this Box enters the viewport
-      ref={setElement}
-      sx={{
-        position: 'relative',
-        aspectRatio: 25 / 36,
-        borderRadius: CONSTANTS.BORDER_RADIUS,
-        overflow: 'hidden',
-      }}>
-      {isInView && (
-        <Image
-          priority
-          style={{
-            objectFit: 'cover',
-            cursor: 'pointer',
-            opacity: !isImageLoaded ? 0 : 100,
-            transition: 'opacity 0.4s ease-in-out',
-          }}
-          fill
-          src={imageUrl}
-          alt={`Image for ${orderItem.product?.name}`}
-          sizes="(min-width: 1210px) 125px, (min-width: 900px) calc(10.88vw - 11px), calc(32.41vw - 30px)"
-          onLoad={() => setIsImageLoaded(true)}
-        />
-      )}
+    <Link
+      href={`/products/${orderItem.product?.category.toLowerCase()}/${orderItem.product?.name
+        .toLowerCase()
+        .split(' ')
+        .join('-')}/${orderItem.product?.productId}`}>
+      <Box
+        // Attach the ref to track when this Box enters the viewport
+        ref={setElement}
+        sx={{
+          position: 'relative',
+          aspectRatio: 25 / 36,
+          borderRadius: CONSTANTS.BORDER_RADIUS,
+          overflow: 'hidden',
+        }}>
+        {isInView && (
+          <Image
+            priority
+            style={{
+              objectFit: 'cover',
+              cursor: 'pointer',
+              opacity: !isImageLoaded ? 0 : 100,
+              transition: 'opacity 0.4s ease-in-out',
+            }}
+            fill
+            src={imageUrl}
+            alt={`Image for ${orderItem.product?.name}`}
+            sizes="(min-width: 1210px) 125px, (min-width: 900px) calc(10.88vw - 11px), calc(32.41vw - 30px)"
+            onLoad={() => setIsImageLoaded(true)}
+          />
+        )}
 
-      {!isImageLoaded ? (
-        <Skeleton
-          height="100%"
-          width="100%"
-          variant="rectangular"
-        />
-      ) : null}
-    </Box>
+        {!isImageLoaded ? (
+          <Skeleton
+            height="100%"
+            width="100%"
+            variant="rectangular"
+          />
+        ) : null}
+      </Box>
+    </Link>
   );
 }
